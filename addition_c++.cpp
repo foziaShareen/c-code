37a8250948efaa2ea9cba267fab3be7a5aab1ded
foziaShareen <66553473+foziaShareen@users.noreply.github.com>
2020-10-04 20:09:02 +0500

    Add files via upload
    addition of two numbers

---
+++ addition_c++.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int firstNumber=0,secondNumber=0,sum=0;
+    cout<<"please enter your first number "<<endl;
+    cin>>firstNumber;
+    cout<<"please enter your second number "<<endl;
+    cin>>secondNumber;
+    sum=firstNumber+secondNumber;
+    cout<<"the sum of your number is = "<<sum;
+}
